f289651c86fed5dab112bb1234af6cf3f3e69fb7
Your Name <your-email@example.com>
2026-04-20 15:54:14 -0400

    Fix linting errors in routes and log_handlers

--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -29,10 +29,12 @@ def init_logging(app, logger_name: str):
     gunicorn_logger = logging.getLogger(logger_name)
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+
     # Make all log formats consistent
-    formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
-    )
+    log_format = "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S %z"
+    formatter = logging.Formatter(log_format, date_format)
+
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
     app.logger.info("Logging handler established")
--- service/routes.py
+++ service/routes.py
@@ -22,7 +22,7 @@ def health():
 ############################################################
 @app.route("/")
 def index():
-    """Returns information abut the service"""
+    """Returns information about the service"""
     app.logger.info("Request for Base URL")
     return jsonify(
         status=status.HTTP_200_OK,
@@ -40,7 +40,9 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = [
+        dict(name=name, counter=count) for name, count in COUNTER.items()
+    ]
 
     return jsonify(counters)
 
@@ -54,7 +56,10 @@ def create_counters(name):
     app.logger.info("Request to Create counter: %s...", name)
 
     if name in COUNTER:
-        return abort(status.HTTP_409_CONFLICT, f"Counter {name} already exists")
+        return abort(
+            status.HTTP_409_CONFLICT,
+            f"Counter {name} already exists"
+        )
 
     COUNTER[name] = 0
 
@@ -75,7 +80,10 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+        )
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -90,7 +98,10 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+        )
 
     COUNTER[name] += 1
 
